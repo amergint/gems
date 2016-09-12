c7f45b660a2d291ac661d196e275f6cabacb1476
Luis Rodriguez <luis@amergint.com>
2016-09-12 08:39:38 -0600

    Update for testing browserify

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import getConfigResponse from './TestDevice0.json';
 import GetConfigResponse from './GemsMessages';
+import request from 'request';
 
 module.exports = {
     targets: "Tlm0/FrameSync0",
     getConfig: sendGetConfig
 };
 
+console.log("LUISSSSS");
+
 function sendGetConfig(target) {
     ////////////////////////////////////////////////////////
     // Code to send the message goes here....///////////////
     ////////////////////////////////////////////////////////
+    console.log("LUIS");
+
     var responseMessage = new GetConfigResponse(target, getConfigResponse.result);
 
     if (responseMessage.result_code == 'success') {
